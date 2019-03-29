ed5265e7a61f0ae5578e5da803d9587e38728065
Benlahcen Ayman <benlahcenayman@ben.local>
2019-03-29 22:04:46 +0100

    Blockchain and API

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./app');
+var Web3 = require('web3');
+const Tx = require('ethereumjs-tx');
+const truffleContract = require('./connection/app.js');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    truffleContract.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"))
+});
