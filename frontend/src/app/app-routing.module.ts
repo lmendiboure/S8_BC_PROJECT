f491631b4e851ae43d2395e148ff9dd1b7c9eba6
ptaffner <ptaffner@enseirb-matmeca.fr>
2019-05-13 14:37:04 +0200

    modif jolie diffusion

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -26,20 +26,21 @@ import { InformationUserResolver } from './resolvers/informationAboutUser.resolv
 
 const routes: Routes = [
   { path: 'inscription', component: AuthentificationComponent},
-  { path: 'userinformation',component: InformationAboutUserComponent, canActivate: [GuardsComponent], resolve: { information: InformationUserResolver}},
-  { path: 'admin', component: AdminComponent,canActivate: [GuardsComponent]},
-  { path: 'admin/manageuser', component: ManageUserComponent, canActivate: [GuardsComponent], resolve: {users: UsersResolver}},
-  { path: 'admin/manageTI', component: ManageTIComponent, canActivate: [GuardsComponent], resolve: { users: UsersResolver }},
-  { path: '', component: HomeComponent, canActivate: [GuardsComponent]},
+  { path: 'userinformation',component: InformationAboutUserComponent /* , canActivate: [GuardsComponent]*//* , resolve: { information: InformationUserResolver} */},
+  { path: 'admin', component: AdminComponent/* , canActivate: [GuardsComponent]*/},
+  { path: 'admin/manageuser', component: ManageUserComponent /*, canActivate: [GuardsComponent]*//* , resolve: {users: UsersResolver} */},
+  { path: 'admin/manageTI', component: ManageTIComponent /* ,canActivate: [GuardsComponent]*//* , resolve: { users: UsersResolver } */},
+  { path: '', component: HomeComponent /*, canActivate: [GuardsComponent]*/},
   { path: 'login', component: LoginComponent },
-  { path: 'profile/:id', component: ProfileComponent, canActivate: [GuardsComponent],resolve: {information: InformationUserResolver}},
-  { path: 'compte', component: CompteComponent, canActivate: [GuardsComponent]},
-  { path: 'entrant', component: EntrantComponent, canActivate: [GuardsComponent]},
-  { path: 'abonnement', component: AbonnementComponent, canActivate: [GuardsComponent] },
-  { path: 'diffusion', component: DiffusionComponent, canActivate: [GuardsComponent] },
-  { path: 'suivre', component: SuivreComponent, canActivate: [GuardsComponent] },
-  { path: 'admin/managerights', component: ManageDiffusionVideoComponent, canActivate: [GuardsComponent]},
-  { path: 'admin/manageissues', component: GestionProblemesComponent, canActivate: [GuardsComponent]},
+  { path: 'profile/:id', component: ProfileComponent /* ,canActivate: [GuardsComponent]*//* ,resolve: {information: InformationUserResolver} */},
+  { path: 'compte', component: CompteComponent /*, canActivate: [GuardsComponent]*/},
+  { path: 'entrant', component: EntrantComponent /*, canActivate: [GuardsComponent]*/},
+  { path: 'abonnement', component: AbonnementComponent /* ,canActivate: [GuardsComponent]*/ },
+  { path: 'diffusion', component: DiffusionComponent /*, canActivate: [GuardsComponent]*/ },
+  { path: 'suivre', component: SuivreComponent /*, canActivate: [GuardsComponent]*/ },
+  { path: 'admin/managerights', component: ManageDiffusionVideoComponent /* , canActivate: [GuardsComponent]*/},
+  { path: 'admin/manageissues', component: GestionProblemesComponent /* ,canActivate: [GuardsComponent]*/},
+
 
 ];
 
